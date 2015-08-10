313ca4b8b48991d1b77bd7a64c36fdae235cbdc2
cy <cosmicy@outlook.com>
2015-08-10 23:58:02 +0800

    第一个自己建立的layout和activity

---
+++ F22_ActivityTest/src/com/example/f22_activitytest/FirstActivity.java
@@ -0,0 +1,13 @@
+package com.example.f22_activitytest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FirstActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.first_layout);
+	}
+
+}
