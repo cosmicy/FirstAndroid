c4838e4de0fa44080a95b1df8f13fe6a98398196
cosmicy <cosmicy@outlook.com>
2015-08-24 15:24:11 +0800

    button1点击显示toast

--- F22_ActivityTest/src/com/example/f22_activitytest/FirstActivity.java
+++ F22_ActivityTest/src/com/example/f22_activitytest/FirstActivity.java
@@ -2,7 +2,11 @@ package com.example.f22_activitytest;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class FirstActivity extends Activity {
 	@Override
@@ -11,6 +15,17 @@ public class FirstActivity extends Activity {
 		//隐藏标题栏,这句代码一定要在setContentView()之前执行
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.first_layout);
+		
+		Button button1 = (Button)findViewById(R.id.btn_1);
+		button1.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Toast.makeText(FirstActivity.this, "you clicked button1", Toast.LENGTH_SHORT).show();
+			}
+			
+		});
 	}
 
 }
