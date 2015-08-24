f099116ffe6397d21acd74b532014626f3ce5191
cosmicy <cosmicy@outlook.com>
2015-08-24 15:12:47 +0800

    隐藏标题栏

--- F22_ActivityTest/src/com/example/f22_activitytest/FirstActivity.java
+++ F22_ActivityTest/src/com/example/f22_activitytest/FirstActivity.java
@@ -2,11 +2,14 @@ package com.example.f22_activitytest;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class FirstActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		//隐藏标题栏,这句代码一定要在setContentView()之前执行
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.first_layout);
 	}
 
